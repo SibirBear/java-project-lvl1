1024471b68d081115bf508a06fd280fca1c76dfd
SibirBear_Linux <c.igor@list.ru>
2021-03-04 17:50:31 +0700

    COMPLETE STEP 3: Fix .gitignore, Add Makefile, Cli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,13 +1,9 @@
 package hexlet.code;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to the Brain Games!" );
+import hexlet.code.Cli;
+
+public class App {
+    public static void main(final String[] args) {
+        Cli.greeting();
     }
 }
